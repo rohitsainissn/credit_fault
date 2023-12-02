1c0fb6bd76dea24dcc323f6119efd5142201e29c
rohitsainissn <rohitsainissngmail.com>
2023-12-02 22:05:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+<<<<<<< HEAD
+    author="iNeuron",
+    author_email="cloud@ineuron.ai",
+=======
+    author="Rohit",
+    author_email="rohitsainidtu@gmail.com",
+>>>>>>> f8db4db (first commit)
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ src/constant/__init__.py
@@ -0,0 +1,20 @@
+import os
+<<<<<<< HEAD
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "rohit_projects"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+=======
+AWS_S3_BUCKET_NAME = "credit_fault"
+MONGO_DATABASE_NAME = "rohit_projects"
+MONGO_COLLECTION_NAME = "credit_fault"
+
+TARGET_COLUMN = "default payment next month"
+>>>>>>> f8db4db (first commit)
+MONGO_DB_URL="mongodb+srv://rohitsainidtu:jairamjai@cluster0.rmtgsmy.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
